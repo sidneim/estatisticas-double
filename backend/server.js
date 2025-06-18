2e1dc67269affe1f1fb3d09d2e3460a5dc1ce865
sidneim <smistura@gmail.com>
2025-06-18 13:31:38 -0300

    - Colocado o grid com o total de cores por hora - Aumento de numero de pedras para 240

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,32 @@ const pool = new Pool({
 // APIs existentes (exemplo Blaze)
 app.get('/api/stats/blaze', async (req, res) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM resultdoubleblaze ORDER BY created_at DESC LIMIT 120');
+    const { rows } = await pool.query('SELECT * FROM resultdoubleblaze ORDER BY created_at DESC LIMIT 240');
+    res.json(rows);
+  } catch (err) {
+    console.error('Erro na consulta Blaze:', err);
+    res.status(500).json({ error: 'Erro interno' });
+  }
+});
+
+// APIs existentes (exemplo Blaze)
+app.get('/api/porhora/blaze', async (req, res) => {
+  try {
+    const { rows } = await pool.query(`
+        SELECT
+            TO_CHAR(created_at, 'HH24:00') AS hora,
+            SUM(CASE WHEN color = 0 THEN 1 ELSE 0 END) AS branco,
+            SUM(CASE WHEN color = 2 THEN 1 ELSE 0 END) AS preto,
+            SUM(CASE WHEN color = 1 THEN 1 ELSE 0 END) AS vermelho
+        FROM
+            resultdoubleblaze
+        WHERE
+            created_at::date = CURRENT_DATE
+        GROUP BY
+            hora
+        ORDER BY
+            hora desc
+    `);
     res.json(rows);
   } catch (err) {
     console.error('Erro na consulta Blaze:', err);
@@ -31,13 +56,39 @@ app.get('/api/stats/blaze', async (req, res) => {
 // API Jonbet
 app.get('/api/stats/jonbet', async (req, res) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM resultdoublejon ORDER BY created_at DESC LIMIT 120');
+    const { rows } = await pool.query('SELECT * FROM resultdoublejon ORDER BY created_at DESC LIMIT 240');
     res.json(rows);
   } catch (err) {
     console.error('Erro na consulta Jonbet:', err);
     res.status(500).json({ error: 'Erro interno' });
   }
 });
+
+// APIs existentes (exemplo Blaze)
+app.get('/api/porhora/jonbet', async (req, res) => {
+  try {
+    const { rows } = await pool.query(`
+        SELECT
+            TO_CHAR(created_at, 'HH24:00') AS hora,
+            SUM(CASE WHEN color = 0 THEN 1 ELSE 0 END) AS branco,
+            SUM(CASE WHEN color = 2 THEN 1 ELSE 0 END) AS preto,
+            SUM(CASE WHEN color = 1 THEN 1 ELSE 0 END) AS vermelho
+        FROM
+            resultdoublejon
+        WHERE
+            created_at::date = CURRENT_DATE
+        GROUP BY
+            hora
+        ORDER BY
+            hora desc
+    `);
+    res.json(rows);
+  } catch (err) {
+    console.error('Erro na consulta Blaze:', err);
+    res.status(500).json({ error: 'Erro interno' });
+  }
+});
+
 
 // Servir arquivos estáticos do frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
